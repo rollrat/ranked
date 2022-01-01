78a430e494da09c33ba8211ee002c86d37b8f010
rollrat <rollrat.cse@gmail.com>
2022-01-01 10:18:44 +0000

    ranked: impl RankedTable::incp

--- include/ranked.h
+++ include/ranked.h
@@ -13,6 +13,7 @@
 #ifndef _RANKED_
 #define _RANKED_
 
+#include <chrono>
 #include <map>
 #include <queue>
 #include <string>
@@ -32,6 +33,20 @@ class RankedItem {
 public:
 };
 
+class RankedPeriodicUnit {
+public:
+  int64_t expireTime;
+  std::string key;
+  int remain;
+
+  RankedPeriodicUnit(int64_t expireTime, std::string key, int remain)
+      : expireTime(expireTime), key(key), remain(remain) {}
+
+  bool operator<(const RankedPeriodicUnit &other) const {
+    return expireTime < other.expireTime;
+  }
+};
+
 class RankedTable {
 public:
   void inc(const std::string &key, int number) {
@@ -42,18 +57,38 @@ public:
     }
   }
 
-  void incp(const std::string &key, int number, int remain);
+  void incp(const std::string &key, int number, int remain) {
+    inc(key, number);
+    minHeap.push(RankedPeriodicUnit(getTimeStamp() + remain, key, number));
+  }
 
-  Box<int> *get(const std::string &key) const {
+  Box<int> get(const std::string &key) {
+    processPeriodic();
+
     auto kv = map.find(key);
     if (kv != map.end()) {
-      return new Box<int>(kv->second);
+      return Box<int>(kv->second);
     }
-    return new Box<int>();
+    return Box<int>();
   }
 
 private:
   std::map<std::string, int> map;
+  std::priority_queue<RankedPeriodicUnit> minHeap;
+
+  int64_t getTimeStamp() {
+    const auto p1 = std::chrono::system_clock::now();
+
+    return std::chrono::duration_cast<std::chrono::seconds>(
+               p1.time_since_epoch())
+        .count();
+  }
+
+  void processPeriodic() {
+    auto now = getTimeStamp();
+    while (!minHeap.empty() && minHeap.top().expireTime < now)
+      minHeap.pop();
+  }
 
   bool existsKey(const std::string &key) { return map.find(key) != map.end(); }
 };
@@ -81,12 +116,12 @@ public:
     }
   }
 
-  Box<int> *get(const std::string &tableName, const std::string &key) const {
+  Box<int> get(const std::string &tableName, const std::string &key) {
     auto table = getTable(tableName);
     if (table != nullptr) {
       return table->get(key);
     }
-    return new Box<int>();
+    return Box<int>();
   }
 
 private:
